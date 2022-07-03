51170ad22b736a02564abe938988287d427888f8
Eden <ecmuys@gmail.com>
2022-07-03 00:23:21 -0400

    Edit computer choice function and add play round function.

--- main.js
+++ main.js
@@ -1,16 +1,35 @@
-let user = 0; // player choice variable
-let comp = Math.floor(Math.random() * 3) + 1; // computer choice variable
-
-function computerPlay() { // computer choice function
-  if (comp == 1) {
-    console.log("Rock");
-    return comp;
-  } else if (comp == 2) {
-    console.log("Paper");
-    return comp;
+function computerPlay() { // declare computer choice function
+  const comp = Math.floor(Math.random() * 3) + 1;
+  if (comp === 1) {
+    return "rock";
+  } else if (comp === 2) {
+    return "paper";
   } else {
-    console.log("Scissors");
-    return comp;
+    return "scissors"
   }
 }
 
+const playerSelection = prompt("Rock, Paper or Scissors?").toLowerCase();
+const computerSelection = computerPlay();
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return "Draw! Play again.";
+  } else if (playerSelection === "rock" && computerSelection === "paper") {
+    return "You lose! Paper beats Rock.";
+  } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    return "You lose! Scissors beats Paper.";
+  } else if (playerSelection === "scissors" && computerSelection === "rock") {
+    return "You lose! Rock beats Scissors.";
+  } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    return "You win the round!";
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    return "You win the round!";
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    return "You win the round!";
+  } else {
+    alert("Invalid selection, try again.");
+  }
+}
+
+console.log(playRound(playerSelection, computerSelection));
